0871440c8915b6097cdfa7918d700563b03d8298
Syed Khaheera <140519655+khaheeraSyed@users.noreply.github.com>
2024-07-31 09:02:09 +0530

    Create main.dart

---
+++ lib/models/lib/lib/screens/lib/widgets/lib/widgets/lib/screens/lib/screens/lib/widgets/lib/screens/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/product_provider.dart';
+import 'providers/cart_provider.dart';
+import 'providers/auth_provider.dart';
+import 'screens/home_screen.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ProductProvider()),
+        ChangeNotifierProvider(create: (context) => CartProvider()),
+        ChangeNotifierProvider(create: (context) => AuthProvider()),
+      ],
+      child: MaterialApp(
+        title: 'E-Commerce App',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
