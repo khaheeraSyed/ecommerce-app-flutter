3d7633117f01cd76fcc1c110599637d4941b728b
Syed Khaheera <140519655+khaheeraSyed@users.noreply.github.com>
2024-07-31 08:58:12 +0530

    Create product_detail_screen.dart

---
+++ lib/models/lib/lib/screens/lib/widgets/lib/widgets/lib/screens/product_detail_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  final Product product;
+
+  ProductDetailScreen({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(product.title)),
+      body: Column(
+        children: [
+          Image.network(product.image),
+          Text(product.title),
+          Text('\$${product.price}'),
+          Text(product.description),
+          Text('${product.rating} ★'),
+          ElevatedButton(
+            onPressed: () {
+              // Add to cart functionality
+            },
+            child: Text('Add to Cart'),
+          ),
+        ],
+      ),
+    );
+  }
+}
